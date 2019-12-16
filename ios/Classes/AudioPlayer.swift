b8d8c4eb31a92f7fd8b0f3db1d22a305948732fc
牛新怀 <niuxinhuai@fanfandata.com>
2019-12-16 15:16:57 +0800

    iOS plugin

---
+++ ios/Classes/AudioPlayer.swift
@@ -0,0 +1,169 @@
+//
+//  AudioPlayer.swift
+//  audio_player
+//
+//  Created by 牛新怀 on 2019/12/12.
+//
+
+import Foundation
+import AVFoundation
+
+public enum AudioPlayerError: Error {
+    case fileExtension, fileNotFound
+}
+
+public class ListenAudioPlayer: NSObject {
+    
+    /// Name of the used to initialize the object
+    public var name: String?
+    
+    /// URL of the used to initialize the object
+    public let url: URL?
+    fileprivate let player: AVAudioPlayer?
+    
+    // MARK: Init
+    
+    public convenience init(fileName: String) throws {
+        let soundFileComponents = fileName.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: ".")
+        guard soundFileComponents.count == 2 else {
+            throw AudioPlayerError.fileExtension
+        }
+        
+        guard let url = Bundle.main.url(forResource: soundFileComponents[0], withExtension: soundFileComponents[1]) else {
+            throw AudioPlayerError.fileNotFound
+        }
+        try self.init(contentsOf: url)
+    }
+    
+    public convenience init(contentsOfPath path: String) throws {
+        let fileURL = URL(fileURLWithPath: path)
+        try self.init(contentsOf: fileURL)
+    }
+    
+    public init(contentsOf url: URL) throws {
+        self.url = url
+        let data = try Data.init(contentsOf: url)
+        name = url.lastPathComponent
+        player = try AVAudioPlayer.init(data: data, fileTypeHint: AVFileType.mp3.rawValue)
+        try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+        try AVAudioSession.sharedInstance().setActive(true)
+        player?.numberOfLoops = 0
+        player?.currentTime = 0
+        player?.prepareToPlay()
+        super.init()
+    }
+}
+
+// MARK: - play/pause/...
+extension ListenAudioPlayer {
+    
+    public var isPlaying: Bool {
+        guard let nonNilsound = player else {
+            return false
+        }
+        return nonNilsound.isPlaying
+    }
+    
+    /// the duration of the player.
+    public var duration: TimeInterval {
+        guard let nonNilsound = player else {
+            return 0.0
+        }
+        return nonNilsound.duration
+    }
+    
+    /// currentTime is the offset into the sound of the current playback position.
+    public var currentTime: TimeInterval {
+        get {
+            guard let nonNilsound = player else {
+                return 0.0
+            }
+            return nonNilsound.currentTime
+        }
+        set {
+            player?.currentTime = newValue
+        }
+    }
+    
+    /// The volume for the sound. The nominal range is from 0.0 to 1.0.
+    public var volume: Float {
+        get {
+            guard let nonNilsound = player else {
+                return 0.0
+            }
+            return nonNilsound.volume
+        }
+        set {
+            player?.volume = newValue
+        }
+    }
+    
+    /* "numberOfLoops" is the number of times that the sound will return to the beginning upon reaching the end.
+     A value of zero means to play the sound just once.
+     A value of one will result in playing the sound twice, and so on..
+     Any negative number will loop indefinitely until stopped.
+     */
+    public var numberOfLoops: Int {
+        get {
+            guard let nonNilsound = player else {
+                return 0
+            }
+            return nonNilsound.numberOfLoops
+        }
+        set {
+            player?.numberOfLoops = newValue
+        }
+    }
+    
+    /* set panning. -1.0 is left, 0.0 is center, 1.0 is right. */
+    public var pan: Float {
+        get {
+            guard let nonNilsound = player else {
+                return 0.0
+            }
+            return nonNilsound.pan
+        }
+        set {
+            player?.pan = newValue
+        }
+    }
+    
+    /* You must set enableRate to YES for the rate property to take effect. You must set this before calling prepareToPlay. */
+    public var enableRate: Bool {
+        get {
+            guard let nonNilsound = player else {
+                return false
+            }
+            return nonNilsound.enableRate
+        }
+        set {
+            player?.enableRate = newValue
+        }
+    }
+    
+     /* See enableRate. The playback rate for the sound. 1.0 is normal, 0.5 is half speed, 2.0 is double speed. */
+    public var rate: Float {
+        get {
+            guard let nonNilsound = player else {
+                return 0.0
+            }
+            return nonNilsound.rate
+        }
+        set {
+            player?.rate = newValue
+        }
+    }
+    
+    public func play() {
+        player?.play()
+    }
+    
+    public func pause() {
+        player?.pause()
+    }
+    
+    public func stop() {
+        player?.stop()
+    }
+    
+}
---
+++ ios/Classes/MethodNamed.swift
@@ -0,0 +1,21 @@
+//
+//  MethodNamed.swift
+//  audio_player
+//
+//  Created by 牛新怀 on 2019/12/12.
+//
+
+import Foundation
+
+struct AudioPlayerMethodCallName {
+    static let inited = "init"
+    static let create = "create"
+    static let setLooping = "setLooping"
+    static let setVolume = "setVolume"
+    static let play = "play"
+    static let pause = "pause"
+    static let seekTo = "seekTo"
+    static let position = "position"
+    static let dispose = "dispose"
+    static let setSpeed = "setSpeed"
+}
